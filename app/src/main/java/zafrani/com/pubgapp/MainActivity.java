1c0051491a2d3b2a1c8429964e421818e692bdcb
Steven Zafrani <zsteven44@gmail.com>
2017-04-30 18:27:28 -0700

    feat: actionbar removed in theme specified in manifest.  design library added to gradle.

--- app/src/main/java/zafrani/com/pubgapp/MainActivity.java
+++ app/src/main/java/zafrani/com/pubgapp/MainActivity.java
@@ -1,7 +1,11 @@
 package zafrani.com.pubgapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import zafrani.com.pubgapp.adapters.MainFragmentPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new MainFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
 
 
