25807133d87a4c9bdc1a4173b3c33fc57c6b5553
zafrani <d.zafrani21@gmail.com>
2017-05-03 20:38:44 -0700

    loading json file

--- app/src/main/java/zafrani/com/pubgapp/fragments/ItemFragment.java
+++ app/src/main/java/zafrani/com/pubgapp/fragments/ItemFragment.java
@@ -2,20 +2,40 @@ package zafrani.com.pubgapp.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 import zafrani.com.pubgapp.R;
+import zafrani.com.pubgapp.models.Items;
+import zafrani.com.pubgapp.utils.FileUtil;
 
 public class ItemFragment extends BaseFragment {
 
     public static String TAG = ItemFragment.class.getSimpleName();
+
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater,
                              @Nullable final ViewGroup container,
                              final Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_item, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        try {
+            Items items = FileUtil.getItems(getActivity());
+            Log.e(getClass()
+                          .getSimpleName(), "items: " + (items == null ? "null" : items.toString()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(getClass().getSimpleName(), "Error: " + e.getLocalizedMessage());
+        }
     }
 }
---
+++ app/src/main/java/zafrani/com/pubgapp/utils/FileUtil.java
@@ -0,0 +1,29 @@
+package zafrani.com.pubgapp.utils;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import zafrani.com.pubgapp.models.Items;
+
+public class FileUtil {
+
+    @Nullable
+    public static Items getItems(@NonNull final Activity activity) throws IOException {
+        final InputStream inputStream = activity.getAssets().open("items.json");
+
+        if (inputStream != null) {
+            final Gson gson = new Gson();
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            return gson.fromJson(reader, Items.class);
+        }
+        return null;
+    }
+}
