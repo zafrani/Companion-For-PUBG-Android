dcddb407b862d7ffce9acb60de3afed6ec552c25
Steven Zafrani <zsteven44@gmail.com>
2017-05-07 15:21:58 -0700

    remeoved unnecessary comments

--- app/src/main/java/tech/zafrani/pubgapp/fragments/ItemFragment.java
+++ app/src/main/java/tech/zafrani/pubgapp/fragments/ItemFragment.java
@@ -30,45 +30,11 @@ public class ItemFragment extends BaseFragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        /*
-        final List<Item> items = new ArrayList<>();
-        try {
-            Items itemList = FileUtil.getItems(getActivity());
-            Log.e(getClass()
-                          .getSimpleName(), "Items contents: " + (itemList == null ? "null" : items.toString()));
-            for (final Category category: itemList.getCategories()) {
-                Log.e(getClass()
-                        .getSimpleName(), "Categories: " + (itemList.getCategories() == null ? "null" : category.toString()));
-                Log.e(getClass()
-                        .getSimpleName(), "Types for category " + category + ": " + (category.getTypes() == null ? "null" : category.getTypes().toString()));
-                if (category.getTypes() != null){
-                    for (final Type type : category.getTypes()) {
-                        Log.e(getClass()
-                            .getSimpleName(), "Items for Type " + type + ": " + (type.getItems() == null ? "null" : type.getItems().toString()));
-                        items.addAll(type.getItems());
-
-                    }
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e(getClass().getSimpleName(), "Error: " + e.getLocalizedMessage());
-        }
-        */
-
-
         final ViewPager viewPager = (ViewPager) view.findViewById(R.id.viewpager);
         viewPager.setAdapter(new ItemTabAdapter(getFragmentManager()));
         final TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tablayout);
         tabLayout.setupWithViewPager(viewPager);
 
-
-        /*
-        recyclerView = (RecyclerView) view.findViewById(R.id.fragment_item_recyclerview);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        final ItemListAdapter adapter =  new ItemListAdapter(items);
-        recyclerView.setAdapter(adapter);
-        */
     }
 
 
