7cafaf1e5a78eb34bf081608fc7290428b9f0166
Steven Zafrani <zsteven44@gmail.com>
2017-05-07 12:52:41 -0700

    feat: tab layout and view pager functional

--- app/src/main/java/tech/zafrani/pubgapp/fragments/ItemTabFragment.java
+++ app/src/main/java/tech/zafrani/pubgapp/fragments/ItemTabFragment.java
@@ -1,8 +1,41 @@
 package tech.zafrani.pubgapp.fragments;
 
-/**
- * Created by stevenzafrani on 5/7/17.
- */
 
-public class ItemTabFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import tech.zafrani.pubgapp.R;
+
+public class ItemTabFragment  extends BaseFragment{
+
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int page;
+
+    public static ItemTabFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        ItemTabFragment fragment = new ItemTabFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_itemtab, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + page);
+        return view;
+    }
 }
+
