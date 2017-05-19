e4f639004c76d6ff71e0165f6e29b81f934c8c12
David Zafrani <d.zafrani21@gmail.com>
2017-05-18 17:56:03 -0700

    clean up

--- app/src/main/java/tech/zafrani/pubgapp/activities/MainActivity.java
+++ app/src/main/java/tech/zafrani/pubgapp/activities/MainActivity.java
@@ -1,14 +1,5 @@
 package tech.zafrani.pubgapp.activities;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-
 public class MainActivity extends DrawerActivity {
-
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-    }
-
 
 }
