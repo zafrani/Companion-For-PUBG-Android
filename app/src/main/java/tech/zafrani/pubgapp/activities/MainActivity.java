4137fd85cbca94bd3e9e12c3b0f2a962289e9479
David Zafrani <d.zafrani21@gmail.com>
2017-05-18 17:50:50 -0700

    Added alert dialog for beta disclaimer

--- app/src/main/java/tech/zafrani/pubgapp/activities/MainActivity.java
+++ app/src/main/java/tech/zafrani/pubgapp/activities/MainActivity.java
@@ -1,8 +1,14 @@
 package tech.zafrani.pubgapp.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
 public class MainActivity extends DrawerActivity {
 
-
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
 
 
 }
--- app/src/main/java/tech/zafrani/pubgapp/fragments/ItemFragment.java
+++ app/src/main/java/tech/zafrani/pubgapp/fragments/ItemFragment.java
@@ -1,9 +1,13 @@
 package tech.zafrani.pubgapp.fragments;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -49,10 +53,31 @@ public class ItemFragment extends BaseFragment {
         viewPager.setAdapter(new ItemTabAdapter(getChildFragmentManager(), categories));
         final TabLayout tabLayout = (TabLayout) view.findViewById(R.id.fragment_item_tablayout);
         tabLayout.setupWithViewPager(viewPager);
+        showDisclaimerForFirstTime();
 
 
 
     }
 
 
+    //todo remove this after data is updated
+    private void showDisclaimerForFirstTime() {
+        final SharedPreferences preferences = getActivity().getSharedPreferences("temp", Context.MODE_PRIVATE);
+        final String hasShown = "hasShown";
+        if (preferences.getBoolean(hasShown, false)) {
+            return;
+        }
+
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity())
+                .setTitle("This is still in beta!")
+                .setMessage("Data is not yet finished nor complete. Please help us via Github. Links available in the drawer.")
+                .setPositiveButton("Ok no problem!", null)
+                .show();
+        alertDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                preferences.edit().putBoolean(hasShown, true).apply();
+            }
+        });
+    }
 }
