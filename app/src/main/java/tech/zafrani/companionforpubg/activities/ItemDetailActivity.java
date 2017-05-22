f0807cd00fceaba8f9ccf38f53b7be07be994de8
Steven Zafrani <zsteven44@gmail.com>
2017-05-21 18:53:24 -0700

    feat: removed all previous itemFragment code

--- app/src/main/java/tech/zafrani/companionforpubg/activities/ItemDetailActivity.java
+++ app/src/main/java/tech/zafrani/companionforpubg/activities/ItemDetailActivity.java
@@ -9,10 +9,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import tech.zafrani.companionforpubg.R;
-import tech.zafrani.companionforpubg.fragments.ItemDetailsFragment;
-import tech.zafrani.companionforpubg.models.Item;
-
-import static tech.zafrani.companionforpubg.adapters.viewholders.ItemViewHolder.ARG_ITEM;
 
 public class ItemDetailActivity extends AppCompatActivity {
 
@@ -23,12 +19,9 @@ public class ItemDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_itemdetails);
 
         Intent intent = getIntent();
-        final Item item = (Item) intent.getSerializableExtra(ARG_ITEM);
-        showItemDetailFragment(new ItemDetailsFragment(), item);
     }
 
-    public void showItemDetailFragment(@NonNull final Fragment fragment,
-                                       @NonNull final Item item){
+    public void showItemDetailFragment(@NonNull final Fragment fragment){
 
     }
 
--- app/src/main/java/tech/zafrani/companionforpubg/adapters/ItemTabAdapter.java
+++ app/src/main/java/tech/zafrani/companionforpubg/adapters/ItemTabAdapter.java
@@ -6,38 +6,29 @@ import android.app.FragmentManager;
 import android.support.annotation.NonNull;
 import android.support.v13.app.FragmentPagerAdapter;
 
-import java.util.List;
-
 import tech.zafrani.companionforpubg.fragments.ItemTabFragment;
-import tech.zafrani.companionforpubg.models.Category;
 
 public class ItemTabAdapter extends FragmentPagerAdapter {
-    private List<Category> categories;
 
-
-
-    public ItemTabAdapter(@NonNull final FragmentManager fm,
-                          @NonNull final List<Category> categories) {
+    public ItemTabAdapter(@NonNull final FragmentManager fm) {
         super(fm);
-        this.categories = categories;
-
     }
 
 
     @Override
     public int getCount() {
-        return categories.size();
+        return 0;
     }
 
     @NonNull
     @Override
     public Fragment getItem(final int position) {
-        return ItemTabFragment.newInstance(categories.get(position));
+        return ItemTabFragment.newInstance();
     }
 
     @NonNull
     @Override
     public CharSequence getPageTitle(final int position) {
-        return categories.get(position).getName();
+        return "";
     }
 }
