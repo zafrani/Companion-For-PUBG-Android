79b9b4b8b8960984f6984fa3ae339d48561d0864
David Zafrani <d.zafrani21@gmail.com>
2017-05-22 15:29:15 -0700

    Clean up

--- app/src/main/java/tech/zafrani/companionforpubg/models/items/Categories.java
+++ app/src/main/java/tech/zafrani/companionforpubg/models/items/Categories.java
@@ -1,31 +1,19 @@
 package tech.zafrani.companionforpubg.models.items;
 
-import android.support.annotation.NonNull;
-
-import com.google.gson.annotations.SerializedName;
-
 import java.util.HashMap;
 
 public class Categories extends HashMap<String, Category> {
 
-    // @SerializedName("weapon_category")
-    //private final WeaponCategory weaponCategory;
-
-    public Categories() {
-
-    }
-
-
     @Override
     public String toString() {
-        String cat = "Categories{";
+        String returnString = "Categories{";
         for (final Entry<String, Category> entry : entrySet()) {
             if (entry == null || entry.getValue() == null) {
                 continue;
             }
-            cat += entry.getKey() + ", " + entry.getValue().toString();
+            returnString += "\n" + entry.getKey() + ", " + entry.getValue().toString();
         }
-        cat += '}';
-        return cat;
+        returnString += " \n}";
+        return returnString;
     }
 }
