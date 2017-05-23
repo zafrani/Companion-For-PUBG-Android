19cd0cd443548c35a8255c41794c4aa4343a7b58
Steven Zafrani <zsteven44@gmail.com>
2017-05-23 10:24:58 -0700

    feat: string constants for scraping added

--- app/src/main/java/tech/zafrani/companionforpubg/utils/PUBGNewsFetch.java
+++ app/src/main/java/tech/zafrani/companionforpubg/utils/PUBGNewsFetch.java
@@ -11,7 +11,29 @@ import java.io.IOException;
 
 import tech.zafrani.companionforpubg.models.NewsItem;
 
+import static tech.zafrani.companionforpubg.utils.Constants.PUBG_NEWS_LINK;
+
 public class PUBGNewsFetch extends AsyncTask<Void, NewsItem, Void>  {
+    @NonNull
+    private final static String NEWS_DOCUMENT_SELECTOR = "ul#allList li";
+    @NonNull
+    private final static String NEWS_TITLE_SELECTOR = ".title";
+    @NonNull
+    private final static String NEWS_TYPE_SELECTOR = ".sp_sub";
+    @NonNull
+    private final static String NEWS_DATE_SELECTOR = ".date";
+    @NonNull
+    private final static String NEWS_DESCRIPTION_SELECTOR = ".descrption";
+    @NonNull
+    private final static String NEWS_LINK_SELECTOR = "a";
+    @NonNull
+    private final static String NEWS_LINKSRC_SELECTOR = "href";
+    @NonNull
+    private final static String NEWS_IMG_SELECTOR = ".img_wrap img";
+    @NonNull
+    private final static String NEWS_IMGSRC_SELECTOR = "src";
+
+
     public PUBGNewsListener delegate = null;
 
     @Override
@@ -19,18 +41,16 @@ public class PUBGNewsFetch extends AsyncTask<Void, NewsItem, Void>  {
 
         final Document document;
         try {
-            document = Jsoup.connect("https://www.playbattlegrounds.com/news.pu").get();
-            for (final Element row : document.select("ul#allList li")) {
-
-                final String title = row.select(".title").text();
-                final String type = row.select(".sp_sub").text();
-                final String date = row.select(".date").text();
-                final String description = row.select(".descrption").text();
-
-                final Element link = row.select("a").first();
-                final String linkSrc = link.absUrl("href");
-                final Element img = row.select(".img_wrap img").first();
-                final String imgSrc = img.absUrl("src");
+            document = Jsoup.connect(PUBG_NEWS_LINK).get();
+            for (final Element row : document.select(NEWS_DOCUMENT_SELECTOR)) {
+                final String title = row.select(NEWS_TITLE_SELECTOR).text();
+                final String type = row.select(NEWS_TYPE_SELECTOR).text();
+                final String date = row.select(NEWS_DATE_SELECTOR).text();
+                final String description = row.select(NEWS_DESCRIPTION_SELECTOR).text();
+                final Element link = row.select(NEWS_LINK_SELECTOR).first();
+                final String linkSrc = link.absUrl(NEWS_LINKSRC_SELECTOR);
+                final Element img = row.select(NEWS_IMG_SELECTOR).first();
+                final String imgSrc = img.absUrl(NEWS_IMGSRC_SELECTOR);
 
                 publishProgress(new NewsItem(title, type, date, description, linkSrc, imgSrc));
             }
